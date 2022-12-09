b8e905a6c389e649f7b7a2317161cfb17cbebd47
JameSs-66 <thanawat_jaisert@cum.ac.th>
2022-12-09 16:29:57 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int num = 1,  oddNum=0,  evenNum=0;
+
+    while(num != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+
+    if(num%2 != 0){
+        oddNum++;
+       
+    }else{
+         evenNum++;
+    }
+    }
+  
+    cout << "#Even numbers = " << evenNum-1 << "\n";
+    cout << "#Odd numbers = " << oddNum;
     return 0;
 }
